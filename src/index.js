2627f08973a8b2238232fcf5327cb52437073f09
Colson <colsoncalifornication@gmail.com>
2024-06-14 14:10:19 +0545

    link up index js and sum js using CommonJS module system

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// Module System
+
+/* @ CommonJS
+ * require, module.exports
+ * */
+
+/* @ AMD (Asynchronous Module Definition)
+ * require, define
+ * */
+
+/* @ ES2015
+ * import, export
+ * */
+
+// CommonJS implementation
+const sum = require('./sum'):
+
+const total = sum(10, 9);
+console.log(total);
+
