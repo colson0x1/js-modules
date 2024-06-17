2e5f434c62f4ad9a58f738695ea403597a63ebbf
Colson <colsoncalifornication@gmail.com>
2024-06-17 16:50:16 +0545

    extract text plugin: tweak loader pipeline to generate a separate css file rather than including css into the actual bundle.js and injecting all the additional code to add a style tag to the HTML document + link generated style.css to index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   entry: './src/index.js',
@@ -14,12 +15,20 @@ const config = {
         test: /\.js$/,
       },
       {
+        /* use: ['style-loader', 'css-loader'], */
+
         // Loaders are applied from RIGHT to LEFT
-        use: ['style-loader', 'css-loader'],
+        // 'use' and 'loader' property are identical in the Webpack world
+        // some plugin requires legacy property name like loader in the case of
+        // ExtractTextPlugin because of the way they are written
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader',
+        }),
         test: /\.css$/,
       },
     ],
   },
+  plugins: [new ExtractTextPlugin('style.css')],
 };
 
 module.exports = config;
