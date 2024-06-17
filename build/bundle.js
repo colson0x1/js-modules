d543dce0b8be091acb0d914bdf99504bca613710
Colson <colsoncalifornication@gmail.com>
2024-06-17 13:48:55 +0545

    module loaders: configure Babel for ES2015 - use Babel loader to transpile JavaScript from ES2015 and higher versions to ES5 + add babelrc file to configure babel preset env module + use Webpack to use big pile of JS modules and merge them all together

--- build/bundle.js
+++ build/bundle.js
@@ -63,42 +63,14 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
 /***/ function(module, exports) {
 
-const sum = (a, b) => a + b;
-
-module.exports = sum;
-
-
-/***/ },
-/* 1 */
-/***/ function(module, exports, __webpack_require__) {
-
-// Module System
-
-/* @ CommonJS
- * require, module.exports
- * */
-
-/* @ AMD (Asynchronous Module Definition)
- * require, define
- * */
-
-/* @ ES2015
- * import, export
- * */
-
-// CommonJS implementation
-const sum = __webpack_require__(0);
-
-const total = sum(10, 9);
-console.log(total);
-
+throw new Error("Module build failed: Error: Cannot find module '@babel/core'\nRequire stack:\n- /home/colson/reactprod/jsModules/node_modules/babel-loader/lib/index.js\n- /home/colson/reactprod/jsModules/node_modules/loader-runner/lib/loadLoader.js\n- /home/colson/reactprod/jsModules/node_modules/loader-runner/lib/LoaderRunner.js\n- /home/colson/reactprod/jsModules/node_modules/webpack/lib/NormalModule.js\n- /home/colson/reactprod/jsModules/node_modules/webpack/lib/NormalModuleFactory.js\n- /home/colson/reactprod/jsModules/node_modules/webpack/lib/Compiler.js\n- /home/colson/reactprod/jsModules/node_modules/webpack/lib/webpack.js\n- /home/colson/reactprod/jsModules/node_modules/webpack/bin/webpack.js\n babel-loader@9 requires Babel 7.12+ (the package '@babel/core'). If you'd like to use Babel 6.x ('babel-core'), you should install 'babel-loader@7'.\n    at Module._resolveFilename (node:internal/modules/cjs/loader:1142:15)\n    at Module._load (node:internal/modules/cjs/loader:983:27)\n    at Module.require (node:internal/modules/cjs/loader:1230:19)\n    at require (node:internal/modules/helpers:179:18)\n    at Object.<anonymous> (/home/colson/reactprod/jsModules/node_modules/babel-loader/lib/index.js:3:11)\n    at Module._compile (node:internal/modules/cjs/loader:1368:14)\n    at Module._extensions..js (node:internal/modules/cjs/loader:1426:10)\n    at Module.load (node:internal/modules/cjs/loader:1205:32)\n    at Module._load (node:internal/modules/cjs/loader:1021:12)\n    at Module.require (node:internal/modules/cjs/loader:1230:19)\n    at require (node:internal/modules/helpers:179:18)\n    at loadLoader (/home/colson/reactprod/jsModules/node_modules/loader-runner/lib/loadLoader.js:18:17)\n    at iteratePitchingLoaders (/home/colson/reactprod/jsModules/node_modules/loader-runner/lib/LoaderRunner.js:169:2)\n    at runLoaders (/home/colson/reactprod/jsModules/node_modules/loader-runner/lib/LoaderRunner.js:365:2)\n    at NormalModule.doBuild (/home/colson/reactprod/jsModules/node_modules/webpack/lib/NormalModule.js:131:2)\n    at NormalModule.build (/home/colson/reactprod/jsModules/node_modules/webpack/lib/NormalModule.js:179:15)\n    at Compilation.buildModule (/home/colson/reactprod/jsModules/node_modules/webpack/lib/Compilation.js:129:9)\n    at Compilation.<anonymous> (/home/colson/reactprod/jsModules/node_modules/webpack/lib/Compilation.js:406:8)\n    at /home/colson/reactprod/jsModules/node_modules/webpack/lib/NormalModuleFactory.js:243:4\n    at /home/colson/reactprod/jsModules/node_modules/webpack/lib/NormalModuleFactory.js:94:13\n    at /home/colson/reactprod/jsModules/node_modules/tapable/lib/Tapable.js:268:11\n    at NormalModuleFactory.<anonymous> (/home/colson/reactprod/jsModules/node_modules/webpack/lib/CompatibilityPlugin.js:52:4)\n    at NormalModuleFactory.applyPluginsAsyncWaterfall (/home/colson/reactprod/jsModules/node_modules/tapable/lib/Tapable.js:272:13)\n    at onDoneResolving (/home/colson/reactprod/jsModules/node_modules/webpack/lib/NormalModuleFactory.js:69:11)\n    at onDoneResolving (/home/colson/reactprod/jsModules/node_modules/webpack/lib/NormalModuleFactory.js:190:6)\n    at process.processTicksAndRejections (node:internal/process/task_queues:77:11)");
 
 /***/ }
 /******/ ]);
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,15 @@ const config = {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      // We add object to designate rule or loader
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+      },
+    ],
+  },
 };
 
 module.exports = config;
