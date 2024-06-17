44bbcdf93722159ae54b85c35268918d7ca390ed
Colson <colsoncalifornication@gmail.com>
2024-06-17 23:51:14 +0545

    code splitting: build for performance with Webpack - use webpack to split bundle.js file into separate individual files and programatically show the bundles in codebase i.e only load certain parts of codebase when needed in some particular part of the application - add button with click handler in index.js file and render an image (i.e load up code) from image_viewer.js file only after button gets clicked: use System import call

--- src/index.js
+++ src/index.js
@@ -16,8 +16,21 @@
 // const sum = require('./sum');
 
 // ES2015 Module System
+/*
 import sum from './sum';
 import './image_viewer';
 
 const total = sum(10, 9);
 console.log(total);
+*/
+
+const button = document.createElement('button');
+button.innerText = 'Click me';
+button.onclick = () => {
+  System.import('./image_viewer').then((module) => {
+    // console.log(module);
+    module.default();
+  });
+};
+
+document.body.appendChild(button);
